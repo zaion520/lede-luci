fcebd8e3890945830d85f802a138a73797513702
Hsing-Wang Liao <kuoruan@gmail.com>
2016-10-22 19:22:29 +0800

    luci-app-shadowsocks: update

---
+++ applications/luci-app-shadowsocks/luasrc/controller/shadowsocks.lua
@@ -0,0 +1,30 @@
+-- Copyright (C) 2016 Jian Chang <aa65535@live.com>
+-- Licensed to the public under the GNU General Public License v3.
+
+module("luci.controller.shadowsocks", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/shadowsocks") then
+		return
+	end
+
+	entry({"admin", "services", "shadowsocks"},
+		alias("admin", "services", "shadowsocks", "general"),
+		_("ShadowSocks"), 10).dependent = true
+
+	entry({"admin", "services", "shadowsocks", "general"},
+		cbi("shadowsocks/general"),
+		_("General Settings"), 10).leaf = true
+
+	entry({"admin", "services", "shadowsocks", "servers-manage"},
+		cbi("shadowsocks/servers-manage"),
+		_("Servers Manage"), 20).leaf = true
+
+	if luci.sys.call("command -v ss-redir >/dev/null") ~= 0 then
+		return
+	end
+
+	entry({"admin", "services", "shadowsocks", "access-control"},
+		cbi("shadowsocks/access-control"),
+		_("Access Control"), 30).leaf = true
+end
